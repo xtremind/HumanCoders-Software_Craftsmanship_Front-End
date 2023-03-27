d5e1eac35f1e1a5f145754a74b3ab01f2ce9ba5c
G. Da Graca <guillaume.dagraca@gmail.com>
2023-03-27 23:39:13 +0200

    remove unused

--- main.js
+++
@@ -1,9 +0,0 @@
-const { Pokemon } = require('./pokemon.js');
-
-let pikachu = new Pokemon(1);
-let salameche = new Pokemon(2);
-let bulbizarre = new Pokemon(3);
-
-console.log(pikachu.speed());
-console.log(salameche.speed());
-console.log(bulbizarre.speed());
